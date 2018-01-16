9f2b10f42496afb756a70f843f302a417220cc83
fengyu <5860018fy>
2018-01-16 15:43:30 +0800

    once

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <span class="area">北京</span>
+      <span class="hot-on" @click="changeClass($event)">正在热映</span>
+      <span class="hot-on" @click="changeClass($event)">即将上映</span>
+    </div>
+    <banner></banner>
+  <router-view/>
+  </div>
+</template>
+
+<script>
+import banner from './components/swiper'
+export default {
+  name: 'App',
+  components: {
+    banner
+  },
+  methods: {
+    changeClass: function (e) {
+      e.target.className = 'active'
+    }
+  }
+
+}
+</script>
+
+<style lang="less">
+  *{
+    padding:0;
+    margin:0;
+  }
+  #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size:12px;
+    .header{
+      width: 100%;
+      height: 40px;
+      line-height:40px;
+      border-bottom:1px solid #f2f2f2;
+      .area{
+        display: inline-block;
+        width:4rem;
+      }
+      .hot-on{
+        display: inline-block;
+        width: 6rem;
+      }
+    }
+ }
+ .active{
+   border-bottom:2px solid #ff4d64;
+ }
+</style>
---
+++ src/components/swiper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="banner-wrapper">
+  </div>
+</template>
+<script>
+export default {
+  name: 'banner'
+}
+</script>
+
+<style lang="less" scoped>
+    #banner-wrapper{
+        width:100%;
+        height: 200px;
+    }
+</style>
